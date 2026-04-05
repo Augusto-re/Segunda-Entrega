691e2299764b5785eef8ffe9c6f1858864dc2cd7
Guss98 <augusto.retamozo@outlook.es>
2026-04-05 11:40:58 -0300

    Agregando ejercicio 2

--- src/functions.py
+++ src/functions.py
@@ -16,3 +16,31 @@ def analizar_texto(texto):
 
 
     return total_lineas, total_palabras, promedio, lineas_mayores_promedio
+
+
+def pasar_a_segundos(duracion):
+    partes = duracion.split(":") #Divide en 2 la duracion y lo pone en una lista. Ejemplo: 2:55 lo divide en [2, 5]
+    minutos = int(partes[0])
+    segundos = int(partes[1])
+
+    return minutos * 60 + segundos
+
+def analizar_canciones(playlist):
+    segundos_totales = 0
+    max_duracion = playlist[0]
+    min_duracion = playlist[0]
+
+    for cancion in playlist:
+        segundos = pasar_a_segundos(cancion["duration"])
+        segundos_totales += segundos
+
+        if(segundos > pasar_a_segundos(max_duracion["duration"])):
+            max_duracion = cancion
+
+        if(segundos < pasar_a_segundos(min_duracion["duration"])):
+            min_duracion = cancion
+    
+    minutos_totales = segundos_totales // 60 #Division entera
+    segundos_restantes = segundos_totales % 60 #Lo que resta
+
+    return minutos_totales, segundos_restantes, max_duracion, min_duracion
